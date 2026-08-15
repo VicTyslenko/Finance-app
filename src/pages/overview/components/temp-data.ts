e4401dd198d25800189b92a7079f2770b09a622f
Victor Tyslenko <vidrums86@gmail.com>
2026-08-15 08:47:28 +0100

    feat: add budgets component

--- src/pages/overview/components/temp-data.ts
+++ src/pages/overview/components/temp-data.ts
@@ -3,9 +3,32 @@ export const kpiCards = [
   { title: "Income", price: "$3,814.25" },
   { title: "Expenses", price: "$1,700.50" },
 ];
+export const budgets = [
+  {
+    name: "Entertainment",
+    value: 50,
+    itemStyle: { color: "#277C78", border: "border-green-800" },
+  },
+  {
+    name: "Bills",
+    value: 750,
+    itemStyle: { color: "#82C9D7", border: "border-cyan-600" },
+  },
+  {
+    name: "Dining Out",
+    value: 75,
+    itemStyle: { color: "#F2CDAC", border: "border-gray-400" },
+  },
+  {
+    name: "Personal Care",
+    value: 100,
+    itemStyle: { color: "#626070", border: "border-yellow-400" },
+  },
+];
+export const budgetSpent = 338;
 export const details = [
-  { title: "Savings",        price: "$159", border: "border-green-800" },
-  { title: "Gift",           price: "$40",  border: "border-cyan-600" },
+  { title: "Savings", price: "$159", border: "border-green-800" },
+  { title: "Gift", price: "$40", border: "border-cyan-600" },
   { title: "Concert ticket", price: "$110", border: "border-gray-400" },
-  { title: "New laptop",     price: "$10",  border: "border-yellow-400" },
+  { title: "New laptop", price: "$10", border: "border-yellow-400" },
 ];
