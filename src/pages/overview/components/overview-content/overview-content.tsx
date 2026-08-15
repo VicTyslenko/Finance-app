d7633b331056f3fc15e34d16770aca7bc24bfd65
Victor Tyslenko <victor.tyslenko@osp-retail.co.uk>
2026-08-15 09:17:10 +0100

    feat: add transactions

--- src/pages/overview/components/overview-content/overview-content.tsx
+++ src/pages/overview/components/overview-content/overview-content.tsx
@@ -1,11 +1,13 @@
 import { Budgets } from "./budgets/budgets";
 import { Pots } from "./pots/pots";
+import { Transactions } from "./transactions/transactions";
 
 export const OverviewContent = () => {
   return (
     <div className="flex flex-wrap gap-4 items-start">
       <Pots />
       <Budgets />
+      <Transactions />
     </div>
   );
 };
